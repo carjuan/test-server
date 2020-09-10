a219e54a4b32b107d2afd1bff7b2de5727d73a8f
Juan Carvajal <pcjvan@gmail.com>
2020-09-09 19:33:06 -0500

    Refactor: change route

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(`${__dirname}/public`));
 
-app.get('/', function (request, response) {
+app.get('./test-folder', function (request, response) {
   response.send('Vane is playing fallguys');
 });
 
