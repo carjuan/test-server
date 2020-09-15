b17822a4f9387f82c36fe6c3e135f3f71a3653a1
Juan Carvajal <pcjvan@gmail.com>
2020-09-14 21:52:14 -0500

    Bug: fix port number

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
-app.use(express.static(`${__dirname}/public`));
+app.set("port", process.env.PORT || 5000);
+app.use(express.static(`${__dirname}`));
 
-app.get('/test-folder', function (request, response) {
-  response.send('Vane is playing fallguys');
+app.get("/", function (request, response) {
+  response.send("Hellow World");
 });
 
-app.listen(app.get('port'), function () {
-  console.log(`Node app is running at localhost: ${app.get('port')}`);
+app.listen(app.get("port"), function () {
+  console.log(`Node app is running at localhost: ${app.get("port")}`);
 });
